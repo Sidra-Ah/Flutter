4a2e43c2baceabdc0cd39c187ed3cd98b90cc2e8
dellw <sidraahmed19@hotmail.com>
2021-06-27 03:20:18 +0500

    login

--- lib/main.dart
+++ lib/main.dart
@@ -8,9 +8,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Text("Sidra Ahmed, Sidrashamasartajahmed@gmail.com"),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          // body: Center(
+          //child: Column(
+          //  children: [
+          //    Text("Login"),
+          //  ],
+          // ),
+          appBar: AppBar(
+            title: Center(child: Text("Login Page")),
+          ),
+          body: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Container(width: 200, child: TextField()),
+                SizedBox(
+                  height: 20,
+                ),
+                Container(width: 200, child: TextField()),
+                SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(onPressed: () {}, child: Text("Login"))
+              ],
+            ),
+          ),
+        ));
   }
 }
